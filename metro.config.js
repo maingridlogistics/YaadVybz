65a1f5c614046e92b7fdabd51fb41da06f3f3281
onspace.ai <onspace.ai@gmail.com>
2026-08-08 08:24:14 -0400

    Update APS environment to production

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Preserve the existing cache version setting.
-config.cacheVersion = 'default-config';
-
-module.exports = config;
